b1bddab55c0da7265c8cc233f4133b5b0b654c4b
박종혁 <crusior96@gmail.com>
2020-05-07 16:04:23 +0900

    Update 8주차 2번.cpp

--- CatMaster/문해기/8주차 2번.cpp
+++ CatMaster/문해기/8주차 2번.cpp
@@ -12,6 +12,7 @@ int main()
 	for (int tc = 0;tc < T;tc++) {
 		string A, B;
 		cin >> A >> B;
+		//공통부분서열의 길이를 지정하기 위해 편의상 문자열 앞에 0을 추가해주는 것이 좋음
 		str1 = '0' + A;
 		str2 = '0' + B;
 
@@ -19,16 +20,20 @@ int main()
 		{
 			for (int j = 0;j < str2.length();j++)
 			{
+				//초기 LCS의 값을 0으로 지정해주고 계속 for문을 실행한다
 				if (i == 0 || j == 0)
 				{
 					LCS[i][j] = 0;
 					continue;
 				}
-
+				
+				//서로 같은 문자를 가진 위치가 나타났다면 제일 최근에 결정된 LCS값에서 1을 더해준다
 				if (str1[i] == str2[j])
 				{
 					LCS[i][j] = LCS[i - 1][j - 1] + 1;
 				}
+				//서로 같은 문자를 가지고 있지 않는 위치가 나타났다면
+				//제일 최근에 결정된 LCS값들 중 LCS[i - 1][j]와 LCS[i][j - 1]중 큰 값을 저장해준다
 				else
 				{
 					if (LCS[i - 1][j] > LCS[i][j - 1])
@@ -42,6 +47,7 @@ int main()
 				}
 			}	
 		}
+		//구하고자하는 최종적인 LCS의 값을 출력한다.
 		cout << LCS[str1.length() - 1][str2.length() - 1] << endl;
 	}
 	
